566477d2e031d974a6e9f0be08ad56879df2e29a
gose <gom0367@gmail.com>
2017-11-01 18:38:14 +0900

    nav bar 수정 및 headerinfo 작업중

--- src/component/header_info.js
+++ src/component/header_info.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import '../App.css';
+// import '../App.css';
 
 class HeaderInfo extends Component {
   render() {
     return (
-      <div className="jumbotron">
-        <h1>Hello, world!</h1>
-        <p>...</p>
-        <button type="button" className="btn btn-danger">Action</button>
+      <div className="header-info">
+        <div className="jumbotron">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h1>Hello, world!</h1>
+                <p>ddddd</p>
+                <button type="button" className="btn btn-outline-secondary">
+                  <span className="fa fa-github" aria-hidden="true"></span>
+                  Github
+                </button>
+              </div>
+              <div className="col">
+                <img alt="profile-img" src="https://avatars1.githubusercontent.com/u/27682015?s=400&u=e4ee0d37f412f33eb20482e1f0542a6cde51c90b&v=4" className="profile-img rounded float-right" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+     
     )
   }
 }
